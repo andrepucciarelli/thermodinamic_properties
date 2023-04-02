a72a78e22ecdc3a1ef7ef26174cc5df20737d29f
andrepucciarelli <andre.pucciarelli22@gmail.com>
2023-04-02 10:37:34 -0300

    Added Calculadora Button on sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -7,18 +7,32 @@ from app import app
 
 from datetime import datetime, date
 import plotly.express as px
-import numpy as np
 import pandas as pd
 
 
-
-
-
-
 # ========= Layout ========= #
-layout = dbc.Col([
-                
-            ])
+layout = dbc.Container([
+    dbc.Card([
+        dbc.CardBody([
+            dbc.Col([
+                dbc.Row([
+                    html.Img(src = '/assets/logo.png', style = {'width': '60%', 'display': 'block', 'margin': 'auto'})
+                ])
+            ], style = {'marginBottom': '30px'}),
+            html.Hr(),
+            dbc.Col([
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Button([html.I(className='fa fa-calculator'), ' \t Calculadora de Propriedades'], href = '/inicio', active=True, style = {'display': 'block', 'margin': 'auto', 'font-size': '25px', 'margin-bottom': '10px', 'width':'90%'})
+                    ])
+                ])
+            ], style = {'marginTop': '30px'})
+
+
+            
+        ])
+    ], style = {'marginTop': '20px', 'height': '90vh'})               
+], fluid = True)
 
 
 
--- myindex.py
+++ myindex.py
@@ -3,10 +3,10 @@ import dash
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
-import plotly.express as px
 
 from app import *
 from components import sidebar
+from components import propriedades
 
 
 
@@ -15,21 +15,21 @@ from components import sidebar
 app.layout = dbc.Container([
     dbc.Col([
         sidebar.layout
-    ], md = 2),
+    ], md = 3),
     dbc.Col([
         dbc.Container(id = 'page_content', fluid = True, style = {'widht': '100%', 'height': '100%'})      
-    ],md = 10, style = {'padding': '0%'})
+    ],md = 9, style = {'padding': '0%'})
 ], fluid=True,)
 
 
 # ========= Callbacks ========== #
 app.callback(
-    Output('page_content', 'childre'),
+    Output('page_content', 'children'),
     Input('url', 'pathname')
 )
 def renderizar_pagina(pathname):
     if pathname == '/' or '/inicio':
-        return home.layout
+        return propriedades.layout
     else:
         return html.H3('Página não existe')
 
